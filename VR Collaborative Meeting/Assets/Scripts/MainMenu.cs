46d6aed000c179d369aca3783dfef382e75ecf19
Petar Perić <petarperic93@gmail.com>
2018-08-14 15:50:31 +0200

    svasta nesto

---
+++ VR Collaborative Meeting/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using TMPro;
+
+public class MainMenu : MonoBehaviour {
+
+    public TextMeshProUGUI text;
+    public GameObject cube;
+
+	// Use this for initialization
+	void Start () {
+
+        text.text = "";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetAxis("Horizontal") > 0)
+            cube.transform.position += Vector3.right * Time.deltaTime;
+
+        if (Input.GetAxis("Horizontal") < 0)
+            cube.transform.position += -Vector3.right * Time.deltaTime;
+
+        if (Input.GetAxis("Vertical") > 0)
+            cube.transform.position += Vector3.up * Time.deltaTime;
+
+        if (Input.GetAxis("Vertical") < 0)
+            cube.transform.position += -Vector3.up * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.Joystick1Button3))
+            cube.transform.position += Vector3.forward * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.Joystick1Button0))
+            cube.transform.position += -Vector3.forward * Time.deltaTime;
+
+        //text.text = "Horizontal " + Input.GetAxis("Horizontal").ToString() + " Vertical " + Input.GetAxis("Vertical").ToString();
+
+        //if (Input.anyKey)
+        //{
+        //    foreach (KeyCode code in Enum.GetValues(typeof(KeyCode)))
+        //    {
+        //      //if (Input.GetKeyDown(code))
+        //      //  text.text = code.ToString();
+
+        //        //if (Input.GetKey(code))
+        //        //    text.text = code.ToString();
+
+
+        //    }
+        //}
+    }
+}
