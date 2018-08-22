95db7f2b6542086104bbd4c2363a37085495644d
Petar Perić <petarperic93@gmail.com>
2018-08-22 22:21:02 +0200

    Soba

---
+++ VR Collaborative Meeting/Assets/Scripts/RoomSrcipt.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class RoomSrcipt : MonoBehaviour {
+
+    public GameObject[] Lights;
+    bool isLightUp = true;
+
+	// Use this for initialization
+	void Start () {
+        Lights = GameObject.FindGameObjectsWithTag("light");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            isLightUp = !isLightUp;
+            foreach(GameObject Light in Lights)
+            {
+                Light.SetActive(isLightUp);
+            }
+        }
+	}
+}
